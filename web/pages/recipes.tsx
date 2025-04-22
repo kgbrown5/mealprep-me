8685018db8cf89146c85bc671254776f7de4f6dc
Katie Brown <kgbro@unc.edu>
2025-04-22 18:52:03 -0400

    starting add new form

--- web/pages/recipes.tsx
+++ web/pages/recipes.tsx
@@ -4,21 +4,154 @@ import { createClient } from '@/utils/supabase/clients/server-props'
 import { AppSidebar } from "@/components/app-sidebar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Plus, Ellipsis } from 'lucide-react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { DialogClose } from '@radix-ui/react-dialog'
+import { recipe } from '@/utils/supabase/models/recipes'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+const formSchema = recipe
+
+export function RecipeForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {},
+  }
+  )
+
+  function saveRecipe(values: z.infer<typeof formSchema>) {
+    console.log("Recipe Saved!")
+    console.log(values)
+    // implement sending data to supabase
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(saveRecipe)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+              <Input placeholder='Recipe Name' />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+              <Input placeholder='Recipe Description' />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="photo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Picture</FormLabel>
+              <FormControl>
+              <Input type="file" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="ingredients"
+          render={({ field }) => (
+            <FormItem>
+              {/* Ingredients input here */}
+              <FormLabel>Ingredients</FormLabel>
+              <FormControl>
+              <Input placeholder="Ingredients..." />
+              {/* ^^ if just a text input */}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="custom_text"
+          render={({ field }) => (
+            <FormItem>
+                    <FormLabel>Details</FormLabel>
+                    <FormControl>
+                    <Textarea placeholder='Details about your recipe...' />
+                    </FormControl>
+                  </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
 
 // pagination or scroll? scroll may be easier
 
 export default function Recipes({ user }: { user: User }) {
+    const formSchema = recipe
+
+    const saveRecipe = () => {
+      console.log("Recipe Saved!")
+      // implement sending data to supabase
+    }
+
     return (
       <SidebarProvider>
         <AppSidebar />
         <main className='w-full h-screen'>
         <SidebarTrigger className='my-[1.5rem] mx-[2.5rem]' />
-          <div className='min-h-[89vh] grid grid-cols-3 grid-rows-2 gap-[2rem] mx-[3rem]'>
+          <div className='min-h-[75vh] grid grid-cols-3 grid-rows-2 gap-[2rem] mx-[3rem]'>
             <div className='col-start-1 col-end-2 row-start-1 row-end-2 flex flex-col justify-between'>
               <h1 className='text-3xl font-bold'>My Recipes</h1>
-              <Button className='w-full'><Plus />Add New</Button>
+              <Dialog>
+                <DialogTrigger className='w-full'><Plus />Add New</DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>New Recipe</DialogTitle>
+                  </DialogHeader>
+                  <RecipeForm />
+                  <DialogFooter>
+                    <DialogClose asChild className='flex justify-between'>
+                      <Button variant="secondary">Cancel</Button>
+                      <Button onClick={saveRecipe}>Save</Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             </div>
             <Card>
               <CardHeader className="flex justify-between">
