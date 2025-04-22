793ce6f8ed57f498fefccb2e0fe8c1f183564ce1
Caroline Bryan <cgbryan@unc.edu>
2025-04-22 12:10:36 -0400

    setting up react query

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,13 +2,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "@/components/ui/sonner"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <main>
         <Component {...pageProps} />
       </main>
       <Toaster />
-    </>
+    </QueryClientProvider>
   );
 }
